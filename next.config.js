3ac4ed736b1895322b9cdcb2e7eca3d30b91416b
akggautamasar <Worksbeyondworks@gmail.com>
2026-08-29 15:10:39 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
   images: {
     remotePatterns: [],
   },
+  webpack: (config) => {
+    // pdfjs-dist (used by react-pdf) probes for the Node "canvas" package in
+    // some code paths that never actually run in the browser. Without this
+    // alias, webpack still tries to bundle it and the build fails.
+    config.resolve.alias.canvas = false;
+    return config;
+  },
 };
 
 module.exports = nextConfig;
