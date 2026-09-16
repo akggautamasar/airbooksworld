e0a1c17dd7d61f4be72e6770f89946c2b81b976d
akggautamasar <Worksbeyondworks@gmail.com>
2026-09-16 19:36:24 +0530

    Route book details back to Carollia experience

--- src/app/book/[id]/layout.tsx
+++ src/app/book/[id]/layout.tsx
@@ -1,7 +1,11 @@
+import { redirect } from "next/navigation";
+
 type Props = {
   children: React.ReactNode;
+  params: { id: string };
 };
 
-export default function BookRouteLayout({ children }: Props) {
+export default function BookRouteLayout({ children, params }: Props) {
+  redirect(`/volume/${params.id}`);
   return children;
 }
